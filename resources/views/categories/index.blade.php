314e06bdef9828a492c4ca5949290db3c716aa6d
Faisal Malik <faisallmalick@gmail.com>
2020-03-12 12:01:58 +0500

    first commit

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="d-flex justify-content-end">
+<a href="{{ route('categories.create') }}" class="btn btn-success mb-2">Add Category</a>
+</div>
+
+<div class="card card-default">
+    <div class="card-header">Categories</div>
+
+<div class="card-body">
+
+    <table class="table">
+
+        <thead>
+
+            <th>Name</th>
+            <th></th>
+
+        </thead>    
+
+
+        <tbody>
+
+            @foreach($categories as $category)
+
+                <tr>
+                    <td>
+                        {{ $category->name }}
+                    </td>
+                    <td>
+                        <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-info btn-sm">Edit</a>
+                    </td>
+                </tr>
+                
+            @endforeach
+
+        </tbody>
+
+    </table>
+
+</div>
+
+</div>
+
+@endsection
